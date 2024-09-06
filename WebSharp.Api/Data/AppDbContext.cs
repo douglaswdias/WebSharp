6a114c068498bcc99f0a0efc5b96572fce9728b2
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-09-05 22:06:17 -0300

    InitialCommit

---
+++ WebSharp.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebSharp.Api.Models;
+
+namespace WebSharp.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    DbSet<Product> Products { get; set; }
+    DbSet<Category> Categories { get; set; }
+}
